651dfe212df597fa6e72109b7dd82e6cc625c402
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-24 15:37:37 -0300

    Formularios

---
+++ DemoMVC/DemoMVC/Controllers/FilmesController.cs
@@ -0,0 +1,21 @@
+﻿using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class FilmesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        public IActionResult Adicionar(Filme filme)
+        {
+            if (ModelState.IsValid) { }
+
+            return View(filme);
+        }
+    }
+}
---
+++ DemoMVC/DemoMVC/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace DemoMVC.ViewComponents
+{
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItensCarrinho { get; set; }
+
+        public CarrinhoViewComponent() 
+        {
+            ItensCarrinho = 3;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItensCarrinho);
+        }
+    }
+}
