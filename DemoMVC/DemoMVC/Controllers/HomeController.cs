c7e03795e78a55bafc75e0919d936772ecc489ce
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-23 23:35:21 -0300

    MC

---
+++ DemoMVC/DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoMVC.Controllers
+{
+
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Route("")]
+        [Route("paginal-inicial")]
+        [Route("paginal-inicial/{id:int}/{categoria:int}")]
+        public IActionResult Index(int id, int categoria)
+        {
+            return View();
+        }
+        [Route("privacidade")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [Route("erro")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DemoMVC/DemoMVC/Models/Filme.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.ConstrainedExecution;
+
+namespace DemoMVC.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O Campo Título é Obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O Título deve ter entre 3 e 60 caracteres")]
+        public string Titulo { get; set; }
+
+        [DataType(DataType.DateTime,ErrorMessage = "Data em formato incorreto")]
+        [Required(ErrorMessage = "O Campo Data é Obrigatório")]
+        [Display(Name ="Data de Lançamento")]
+        public DateTime DataLancamento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$",ErrorMessage ="Genero em formato inválido")]
+        [StringLength(60,MinimumLength =3,ErrorMessage = "O Genero deve ter entre 3 e 60 caracteres"),Required(ErrorMessage = "O Campo Genero é requirido")]
+        public string Genero { get; set; }
+        [Required(ErrorMessage = "Preencha o Campo Valor")]
+        [Range(1,1000,ErrorMessage ="Valor de 1 a 1000")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Valor { get; set; }
+        [Required(ErrorMessage ="Preencha o Campo Avaliação")]
+        [Display(Name = "Avaliação")]
+        [RegularExpression(@"^[0-5]*$",ErrorMessage ="Apenas números de 1 a 5")]
+        public int Avaliacao { get; set; }
+
+
+    }
+}
