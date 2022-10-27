3099515dc97962138bcbcac199b3f03cfa6b308d
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-26 21:27:05 -0300

    Areas MVC

---
+++ AppModelo/DevIO.UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Areas.Produtos.Controllers
+{
+    [Area("Produtos")]
+    [Route("Produtos")]
+    public class CadastroController : Controller
+    {
+        [Route("Lista")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public IActionResult Busca()
+        {
+            return View();
+        }
+    }
+}
--- AppModelo/DevIO.UI.Site/Program.cs
+++ AppModelo/DevIO.UI.Site/Program.cs
@@ -1,22 +1,54 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+
+// Builder principal é dele de onde tudo deriva
 var builder = WebApplication.CreateBuilder(args);
 
+// *** Configurando serviços no container ***
+
+// Adicionando suporte a mudança de convenção da rota das areas.
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.AreaViewLocationFormats.Clear();
+    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+});
+
+
+
+// Adicionando suporte ao MVC
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
+// *** Configurando o resquest dos serviços no pipeline ***
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();    
+    
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.MapControllerRoute(
-    name:"default",
-    pattern:"{controller=Home}/{action=Index}/{id?}"
-    );
+// Adicionando suporte a rota
+app.UseRouting();
+
+// Rota padrão
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Rota de área genérica (não necessário no caso da demo)
+//app.MapControllerRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+// Rota de áreas especializadas
+app.MapAreaControllerRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+app.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{action=Index}/{id?}");
 
 app.Run();
-
-
-
